d631d3d256e82c71925ccc1334d899aaa9d98f6d
Susana-Gutierrez <susanagabriela_ap@hotmail.com>
2021-04-28 18:50:13 -0700

    Completed prep-javascript-arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,17 @@
+var languagesArray = ['html', 'css', 'javascript', 'react', 'node.js'];
+console.log('Languages Array', languagesArray);
+console.log('Array length', languagesArray.length);
+languagesArray.push('c#', 'php');
+console.log('Updated Array', languagesArray);
+languagesArray.pop();
+console.log('Last index removed', languagesArray);
+languagesArray.unshift('c++');
+console.log('Added element to front', languagesArray);
+languagesArray.shift();
+console.log('Removed first element', languagesArray);
+var thirdElement = languagesArray[2];
+console.log('3rd element in languagesArray', thirdElement);
+var arrayLength = languagesArray.length;
+console.log('The final length of the array is', arrayLength);
+var lastItem = languagesArray[languagesArray.length - 1];
+console.log('The last item in teh array is ', lastItem);
